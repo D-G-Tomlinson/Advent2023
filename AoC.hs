20e17bb525e5851ec365e9a366af3316a7b53968
D. Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2023-12-01 20:43:58 +0000

    improved

--- AoC.hs
+++ AoC.hs
@@ -4,59 +4,57 @@ import System.IO
 day1Part1 :: IO ()
 day1Part1 = do
   answer <- dod1p1
-  (putStrLn.show) answer
+  print answer
   where
     dod1p1 :: IO Int
     dod1p1 = do
       file <- readFile "textFiles/day1"
-      let dataLines = lines file
-      return (foldr1 (+) (map findNum dataLines))
+      return (sum (map findNum (lines file)))
         where
           findNum :: String -> Int
           findNum (c:s)
-                  | isDigit c = (10 * (digitval c)) + (findLastDigitNum (digitval c) s)
+                  | isDigit c = 10 * digitval c + findLastDigitNum (digitval c) s
                   | otherwise = findNum s
             where
               findLastDigitNum :: Int -> String -> Int
               findLastDigitNum result [] = result
               findLastDigitNum result (c:s)
                       | isDigit c = findLastDigitNum (digitval c)  s
-                      | otherwise = findLastDigitNum (result) s
+                      | otherwise = findLastDigitNum result s
               digitval :: Char -> Int
-              digitval n = (ord n) -48
+              digitval n = ord n - 48
 
 day1Part2 :: IO ()
 day1Part2 = do
   answer <- dod1p2
-  (putStrLn.show) answer
+  print answer
   where
     dod1p2 :: IO Int
     dod1p2 = do
       file <- readFile "textFiles/day1"
-      let dataLines = lines file
-      return (foldr1 (+) (map findNum dataLines))
+      return (sum (map findNum (lines file)))
         where
           findNum :: String -> Int
           findNum (c:s)
-                  | val /= -1 = (10 * val) + (findLastDigitNum val s)
+                  | val /= -1 = 10 * val + findLastDigitNum val s
                   | otherwise = findNum s
             where
               val = digitVal (c:s)
               digitVal :: String -> Int
-              digitVal ('o':'n':'e':_) = 1
-              digitVal ('t':'w':'o':_) = 2
+              digitVal ('o':'n':'e':_)         = 1
+              digitVal ('t':'w':'o':_)         = 2
               digitVal ('t':'h':'r':'e':'e':_) = 3
-              digitVal ('f':'o':'u':'r':_) = 4
-              digitVal ('f':'i':'v':'e':_) = 5
-              digitVal ('s':'i':'x':_) = 6
+              digitVal ('f':'o':'u':'r':_)     = 4
+              digitVal ('f':'i':'v':'e':_)     = 5
+              digitVal ('s':'i':'x':_)         = 6
               digitVal ('s':'e':'v':'e':'n':_) = 7
               digitVal ('e':'i':'g':'h':'t':_) = 8
-              digitVal ('n':'i':'n':'e':_) = 9
+              digitVal ('n':'i':'n':'e':_)     = 9
               digitVal (c:s)
-                      | isDigit c = (ord c) -48
-                      | otherwise = -1
+                      | isDigit c              = ord c - 48
+                      | otherwise              = -1
               findLastDigitNum :: Int -> String -> Int
-              findLastDigitNum result [] = result
+              findLastDigitNum result []     = result
               findLastDigitNum result (c:s)
-                      | (digitVal (c:s)) /= -1 = findLastDigitNum (digitVal (c:s)) s
-                      | otherwise = findLastDigitNum (result) s
+                      | digitVal (c:s) /= -1 = findLastDigitNum (digitVal (c:s)) s
+                      | otherwise            = findLastDigitNum result s
