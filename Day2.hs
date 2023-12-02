595e5470ea9691257294f373c4c40997b1751667
D. Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2023-12-02 19:22:58 +0000

    day2 part2

--- Day2.hs
+++ Day2.hs
@@ -16,16 +16,41 @@ part1 = do
                    | all checkRound (splitOn "; " rest) = id
                    | otherwise                         = 0
               where
-                rest :: String
                 ['G':'a':'m':'e':num,rest] = splitOn ": " gl
                 id = read num
                 checkRound :: String -> Bool
                 checkRound round = all checkCubePairs (joinNumColour (filter (not.null) (splitOneOf ", " round)))
                   where
                     joinNumColour :: [String] -> [(Int, Char)]
-                    joinNumColour [] = []
+                    joinNumColour []          = []
                     joinNumColour (a:(b:_):s) = (read a, b) : joinNumColour s
                     checkCubePairs :: (Int, Char) -> Bool
                     checkCubePairs (n, 'r') = n <= 12
                     checkCubePairs (n, 'g') = n <= 13
                     checkCubePairs (n, 'b') = n <= 14
+part2 :: IO ()
+part2 = do
+  answer <- doP2
+  print answer
+   where
+     doP2 :: IO Int
+     doP2 = do
+        fileText <- readFile "textFiles/day2"
+        return (foldl (\a b -> a + getLineVal b) 0 (lines fileText))
+          where
+            getLineVal :: String -> Int 
+            getLineVal gl = r * g *b
+              where
+                (_:consider:[]) = splitOn ": " gl
+                pairs        = (joinNumColour (filter (not.null) (splitOneOf ", " consider)))
+                  where
+                    joinNumColour :: [String] -> [(Int, Char)]
+                    joinNumColour []          = []
+                    joinNumColour (a:(b:_):s) = (read a, b) : joinNumColour s
+                (r,g,b) = foldl checkMax (0, 0, 0) pairs
+                  where
+                    checkMax :: (Int, Int, Int) -> (Int, Char) -> (Int, Int, Int)
+                    checkMax (r,g,b) (n, 'r') = (max r n, g, b)
+                    checkMax (r,g,b) (n, 'g') = (r, max g n, b)
+                    checkMax (r,g,b) (n, 'b') = (r, g, max b n)
+              
